a3d19318dba9e72db8fdd653541ea65c67dad4b8
Devon Jones <devon.shun.jones@gmail.com>
2026-02-04 08:41:53 -0600

    Fixed some minor Typescript errors

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -12,7 +12,7 @@ import lightLinesLeft from "../../assets/linedesignlightleft.png";
 import { useFetch } from "../../hooks/useFetch";
 import type { ProjectItemProps, TasksResponse } from "../../types";
 import { useAuth } from "../../hooks/useAuth";
-import { Card, Button, Container, Row, Col, Image} from "react-bootstrap";
+import { Card, Button, Container, Row, Col, Image } from "react-bootstrap";
 import { HourglassSplit } from "react-bootstrap-icons";
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -35,11 +35,11 @@ export default function HomePage() {
 
   if (!isAuthenticated) {
     return (
-      <Container className="vh-100 vw-100 d-flex justify-content-center align-items-center" fluid>
-        <Card
-          style={{ width: "18rem" }}
-          className="text-center"
-        >
+      <Container
+        className="vh-100 vw-100 d-flex justify-content-center align-items-center"
+        fluid
+      >
+        <Card style={{ width: "18rem" }} className="text-center">
           <Card.Body>
             <Card.Title>Please Log In</Card.Title>
             <Button variant="primary" onClick={() => navigate("/")}>
@@ -54,41 +54,43 @@ export default function HomePage() {
   return (
     <Container fluid>
       <Row className="m-auto pt-5">
-          <Image
-            className="hero-img"
-            src={isDarkMode ? lightLogo : darkLogo}
-            alt="Quire Logo which is a book with a quill writing in it, with the name Quire above it"
-            fluid
-          />
-            <h2>{`Welcome, ${localStorage.getItem("username")}`}</h2>
-            <h4>{currentDate}</h4>
-            <LiveClock />
+        <Image
+          className="hero-img"
+          src={isDarkMode ? lightLogo : darkLogo}
+          alt="Quire Logo which is a book with a quill writing in it, with the name Quire above it"
+          fluid
+        />
+        <h2>{`Welcome, ${localStorage.getItem("username")}`}</h2>
+        <h4>{currentDate}</h4>
+        <LiveClock />
       </Row>
       <Row>
         <Col className="border-top border-bottom border-end p-3 text-center">
           <Sidebar />
         </Col>
         <Col xs lg="6" className="border-top border-bottom border-end p-3">
-            <div className="d-flex justify-content-center">
-              <Image
-                className="display-list-img"
-                src={isDarkMode ? lightLinesRight : darkLinesRight}
-                alt="Decorative Lines"
-              />
-              <h1>Active Project</h1>
-              <Image
-                className="display-list-img"
-                src={isDarkMode ? lightLinesLeft : darkLinesLeft}
-                alt="Decorative Lines"
-              />
+          <div className="d-flex justify-content-center">
+            <Image
+              className="display-list-img"
+              src={isDarkMode ? lightLinesRight : darkLinesRight}
+              alt="Decorative Lines"
+            />
+            <h1>Active Project</h1>
+            <Image
+              className="display-list-img"
+              src={isDarkMode ? lightLinesLeft : darkLinesLeft}
+              alt="Decorative Lines"
+            />
+          </div>
+          <div className="text-center">
+            <div>
+              <h1>{project?.title}</h1>
+              <p>{project?.description}</p>
+              {project?.dueDate && (
+                <p>{`Due: ${new Date(project.dueDate).toLocaleDateString("en-US")}`}</p>
+              )}
             </div>
-            <div className="text-center">
-              <div>
-                <h1>{project?.title}</h1>
-                <p>{project?.description}</p>
-                <p>{`Due: ${new Date(project!.dueDate).toLocaleDateString("en-US")}`}</p>
-              </div>
-              </div>
+          </div>
           <h3>Tasks for this Project</h3>
           <div
             style={{
@@ -105,7 +107,7 @@ export default function HomePage() {
                   key={task._id}
                   style={{ width: "50rem", height: "9rem" }}
                   className="hover-card mb-3"
-                  onClick={()=>navigate(`/tasks/${task._id}`)}
+                  onClick={() => navigate(`/tasks/${task._id}`)}
                 >
                   <Card.Title className="mt-2 mx-2 d-flex justify-content-between align-items-center">
                     <span>{task.title}</span>
@@ -129,8 +131,8 @@ export default function HomePage() {
           </div>
         </Col>
         <Col className="border-top border-bottom border-end p-3 text-center">
-          <HourglassSplit/>
-          <PomodoroTimer/>
+          <HourglassSplit />
+          <PomodoroTimer />
         </Col>
       </Row>
     </Container>
